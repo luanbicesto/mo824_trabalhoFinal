4fb8a4c2e8b79e185a23c2d1be685c86d4d9061e
Luan Gustavo Bicesto <luanb@LINUXCIT010905.openair4G.eur>
2017-05-30 16:55:57 -0300

    adjust generator

--- src/helper/ConvertInstance.java
+++ src/helper/ConvertInstance.java
@@ -7,6 +7,6 @@ import common.InstanceManager;
 public class ConvertInstance {
     public static void main(String[] args) throws IOException {
         InstanceManager manager = new InstanceManager();
-        manager.convertInstance("TRP-S10-R2");
+        manager.convertInstance("TRP-S10-R1");
     }
 }
