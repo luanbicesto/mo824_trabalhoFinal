f141aaf65cbe5517fec00a66cfcba0e327614390
Luan Gustavo Bicesto <luanb@LINUXCIT010905.openair4G.eur>
2017-06-03 19:09:56 -0300

    PMX implemented

--- src/metaheuristic/ga/TRP_GA.java
+++ src/metaheuristic/ga/TRP_GA.java
@@ -3,7 +3,9 @@ package metaheuristic.ga;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
@@ -25,8 +27,8 @@ public class TRP_GA {
     private enum LS_TYPE {BEST_IMPROVING, FIRST_IMPROVING}
     
     private static int CROSSPOINT_SIZE = 4;
-    private static double NEW_POPULATION_PERCENTAGE = 0.1;
-    private static double HYBRID_POPULATION_PERCENTAGE = 0.2;
+    private static double NEW_POPULATION_PERCENTAGE = 0.0;
+    private static double HYBRID_POPULATION_PERCENTAGE = 0.0;
     
     public TRP_GA(int popSize, int chromosomeSize, int generations, double mutatationRate, Instance instance) {
         this.popSize = popSize;
@@ -210,14 +212,77 @@ public class TRP_GA {
             Chromosome parent1 = parents.get(i);
             Chromosome parent2 = parents.get(i+1);
             
-            crossoverTwoPoints(parent1, parent2, offspring);
+            //crossoverTwoPoints(parent1, parent2, offspring);
+            crossoverPMX(parent1, parent2, offspring);
         }
         
         return offspring;
     }
     
-    private void crossoverOX(Chromosome parent1, Chromosome parent2, Population offspring) {
+    private void crossoverPMX(Chromosome parent1, Chromosome parent2, Population offspring) {
+        int crosspoint1 = rng.nextInt(chromosomeSize);
+        int crosspoint2 = crosspoint1 + CROSSPOINT_SIZE < chromosomeSize ? crosspoint1 + CROSSPOINT_SIZE : crosspoint1;
+        ArrayList<Integer> swath1 = new ArrayList<>();
+        ArrayList<Integer> swath2 = new ArrayList<>();
         
+        Chromosome child1 = new Chromosome(parent1);
+        Chromosome child2 = new Chromosome(parent2);
+        
+        for(int i = crosspoint1; i <= crosspoint2; i++) {
+            swath1.add(child1.get(i));
+            swath2.add(child2.get(i));
+        }
+        
+        Map<Integer, Integer> pmxMapping = createPMXMapping(swath1, swath2);
+        pmxAdjustiment(parent2, child1, pmxMapping, crosspoint1, crosspoint2);
+        pmxAdjustiment(parent1, child2, pmxMapping, crosspoint1, crosspoint2);
+        addChildToOffspring(child1, offspring);
+        addChildToOffspring(child2, offspring);
+    }
+    
+    private void addChildToOffspring(Chromosome child, Population offspring) {
+        child.setFitnessValue(evaluateFitness(child));
+        offspring.add(child);
+    }
+    
+    private void pmxSetGene(Chromosome parent, Chromosome child, Map<Integer, Integer> mapping, int index) {
+        Integer mappedGene = parent.get(index);
+        if(mapping.containsKey(mappedGene)) {
+            mappedGene = mapping.get(mappedGene);
+        }
+        child.set(index, mappedGene);
+    }
+    
+    private void pmxAdjustiment(Chromosome parent, Chromosome child, Map<Integer, Integer> mapping, int crosspoint1, int crosspoint2) {
+        for(int i = 0; i < crosspoint1; i++) {
+            pmxSetGene(parent, child, mapping, i);
+        }
+        
+        for(int i = crosspoint2 + 1; i < parent.size(); i++) {
+            pmxSetGene(parent, child, mapping, i);
+        }
+    }
+    
+    private Map<Integer, Integer> createPMXMapping(ArrayList<Integer> swath1, ArrayList<Integer> swath2) {
+        Map<Integer, Integer> mapping = new HashMap<>();
+        
+        for(int i = 0; i < swath1.size(); i++) {
+            if(swath2.indexOf(swath1.get(i)) == -1) {
+                int initialGene = swath1.get(i);
+                int lastGene = swath2.get(i);
+                
+                int position = swath1.indexOf(lastGene);
+                while(position != -1) {
+                    lastGene = swath2.get(position);
+                    position = swath1.indexOf(lastGene);
+                }
+                
+                mapping.put(initialGene, lastGene);
+                mapping.put(lastGene, initialGene);
+            }
+        }
+        
+        return mapping;
     }
     
     private void crossoverRandom(Chromosome parent1, Chromosome parent2, Population offspring) {
